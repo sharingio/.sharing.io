df50f4c14a31d8c5e01ee7db4bdd69ef4c25904c
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-12-08 09:35:37 +1300

    Update variable fallback

--- sharingio-pair-preinit-script.sh
+++ sharingio-pair-preinit-script.sh
@@ -2,7 +2,7 @@
 
 cat << EOF >> $HOME/.gitconfig
 [credential "https://github.com"]
-  helper = "!f() { test \"\$1\" = get && echo \"password=\$GITHUB_TOKEN\nusername=\$SHARINGIO_PAIR_USER\";}; f"
+  helper = "!f() { test \"\$1\" = get && echo \"password=\${GITHUB_TOKEN:-}\nusername=\${SHARINGIO_PAIR_USER:-}\";}; f"
 EOF
 git config --global commit.template $HOME/.git-commit-template
 cat << EOF > $HOME/.git-commit-template
@@ -10,7 +10,7 @@ cat << EOF > $HOME/.git-commit-template
 
 
 EOF
-for GUEST_NAME in $SHARINGIO_PAIR_GUEST_NAMES; do
+for GUEST_NAME in ${SHARINGIO_PAIR_GUEST_NAMES:-}; do
     echo "Co-Authored-By: $GUEST_NAME <$GUEST_NAME@users.noreply.github.com>" >> $HOME/.git-commit-template
 done
 (
@@ -22,12 +22,12 @@ if [ ! -d /home/ii/.doom.d ]; then
 fi
 
 DOOM_CONFIG_FILE=ii.org
-if [ -f "${HOME}/.doom.d/${SHARINGIO_PAIR_USER}.org" ]; then
-    DOOM_CONFIG_FILE="${SHARINGIO_PAIR_USER}.org"
+if [ -f "${HOME}/.doom.d/${SHARINGIO_PAIR_USER:-}.org" ]; then
+    DOOM_CONFIG_FILE="${SHARINGIO_PAIR_USER:-}.org"
 fi
-if [ -f "${HOME}/.doom.d/${DOOM_CONFIG_FILE}" ]; then
+if [ -f "${HOME}/.doom.d/${DOOM_CONFIG_FILE:-}" ]; then
     rm -f "${HOME}"/.doom.d/*.el
-    org-tangle "${HOME}/.doom.d/${DOOM_CONFIG_FILE}"
+    org-tangle "${HOME}/.doom.d/${DOOM_CONFIG_FILE:-}"
     doom sync
 fi
 
